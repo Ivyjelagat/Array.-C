6e222781f7ea91eccf4a838641bd7ca01dec856c
Ivyjelagat <ivykemboi005@gmail.com>
2024-10-12 11:02:12 +0300

    Create 1D array.c

---
+++ 1D array.c
@@ -0,0 +1,19 @@
+//Array in C- 1D array
+/*
+Author Ivy kemboi
+Date 12/10/2024
+Reg no CT101/G/24882/24
+*/
+
+#include <stdio.h>
+
+int main(){
+	int i;
+	int scores [10]={50,45,30,22,74,94,66,56,86,90};
+	for(i=0; i<10; i++){
+		printf("scores [%d]= %d \n" , i ,scores [i]);
+	}
+	
+	return 0;
+		
+}
